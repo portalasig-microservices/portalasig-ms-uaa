a680540d5951f336ab8890789cd7962357d01fb4
Frank Ponte <fponte@auction.com>
2024-04-03 23:15:21 -0400

    Initial microservice commit

---
+++ src/test/java/com/portalasig/ms/uaa/PortalasigMsUaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portalasig.ms.uaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalasigMsUaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
