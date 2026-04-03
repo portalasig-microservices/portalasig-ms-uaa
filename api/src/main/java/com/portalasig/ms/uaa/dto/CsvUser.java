91de76b836a91c826c1028c88dfcddd1b20bf773
Your Name <you@example.com>
2026-04-03 18:17:04 -0400

    refactor: reorganize CsvUser fields and update their positions

--- api/src/main/java/com/portalasig/ms/uaa/dto/CsvUser.java
+++ api/src/main/java/com/portalasig/ms/uaa/dto/CsvUser.java
@@ -26,13 +26,9 @@ import lombok.NoArgsConstructor;
 @ApiModel(description = "Details about the CSV user")
 public class CsvUser {
 
-    @ApiModelProperty(value = "The email of the user")
-    @CsvBindByPosition(position = 5)
-    private String email;
-
-    @ApiModelProperty(value = "The encoded password of the user")
-    @CsvBindByPosition(position = 2)
-    private String password;
+    @ApiModelProperty(value = "The unique identity of the user")
+    @CsvBindByPosition(position = 1)
+    private Long identity;
 
     @ApiModelProperty(value = "The first name of the user")
     @CsvBindByPosition(position = 3)
@@ -42,27 +38,27 @@ public class CsvUser {
     @CsvBindByPosition(position = 4)
     private String lastName;
 
-    @ApiModelProperty(value = "The unique identity of the user")
-    @CsvBindByPosition(position = 1)
-    private Long identity;
+    @ApiModelProperty(value = "The email of the user")
+    @CsvBindByPosition(position = 5)
+    private String email;
 
-    @ApiModelProperty(value = "The role of the user")
-    @CsvBindByPosition(position = 10)
-    private String role;
+    @ApiModelProperty(value = "Indicates whether the user is active")
+    @CsvBindByPosition(position = 6)
+    private boolean active;
 
     @ApiModelProperty(value = "The date when the user was created")
-    @CsvBindByPosition(position = 8)
+    @CsvBindByPosition(position = 7)
     private String createdDate;
 
     @ApiModelProperty(value = "The date when the user was last updated")
-    @CsvBindByPosition(position = 9)
+    @CsvBindByPosition(position = 8)
     private String updatedDate;
 
-    @ApiModelProperty(value = "Indicates whether the user is active")
-    @CsvBindByPosition(position = 7)
-    private boolean active;
+    @ApiModelProperty(value = "The role of the user")
+    @CsvBindByPosition(position = 9)
+    private String role;
 
     @ApiModelProperty(value = "Email settings of the user as an integer-expressed bitmask")
-    @CsvBindByPosition(position = 11)
+    @CsvBindByPosition(position = 10)
     private Integer emailSettings;
 }
